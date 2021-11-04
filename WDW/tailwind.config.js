6d73389e6ccbacad2e0b254659a3dff2233d9f2b
HabibieF78 <92445196+HabibieF78@users.noreply.github.com>
2021-11-04 17:00:45 +0700

    Add files via upload

---
+++ WDW/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    spacing: {
+      '1': '12px',
+      '2': '16px',
+      '3': '24px',
+      '4': '32px',
+      '5': '48px',
+      '6': '64px',
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
